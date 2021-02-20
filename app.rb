75701e34aab7132b30522599a46fa7c34d2be843
OPR-A-RUBin <opr.a.ruby@gmail.com>
2021-02-20 21:50:22 +0400

    Create base algoritm
    Сочиняем базовый алгоритм нашего приложения. Основные действия.

---
+++ app.rb
@@ -0,0 +1,13 @@
+# Программа сбора информации о файлах
+
+puts 'Start application'
+
+puts 'Query folder adress'
+
+puts 'Make folders and files tree. Create data hach'
+
+puts 'Safe hach information to file'
+
+puts 'Analiz informations from hach'
+
+puts 'Display rezult'
