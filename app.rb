da53252d67751c19cd4e338180a62d207fcbcf00
OPR-A-RUBin <opr.a.ruby@gmail.com>
2021-02-20 22:24:22 +0400

    Refactoring app.rb

--- app.rb
+++ app.rb
@@ -1,17 +1,17 @@
 # Программа сбора информации о файлах
 require './helper.rb'
 
+system('clear')
+puts '0__Start application'
 
-puts 'Start application'
-
-puts 'Query folder adress'
+puts '1__Query folder adress'
 get_path_folder
-puts @path_folder
+puts "Получен путь: #{@path_folder}"
 
-puts 'Make folders and files tree. Create data hach'
+puts '2__Make folders and files tree. Create data hach'
 
-puts 'Safe hach information to file'
+puts '3__Safe hach information to file'
 
-puts 'Analiz informations from hach'
+puts '4__Analiz informations from hach'
 
-puts 'Display rezult'
+puts '5__Display rezult'
