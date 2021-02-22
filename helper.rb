eb0314169f1e8d3b01a12820caec9ea8b534eb94
OPR-A-RUBin <opr.a.ruby@gmail.com>
2021-02-22 22:14:36 +0400

    Move all z_....rb - file from ./ to ./def/
    Перенос всех файлов z_....rb из корневой папки приложения ./ в директорию ./def/

--- helper.rb
+++ helper.rb
@@ -1,8 +1,8 @@
 # 
 
 # Это мои процедуры для работы приложения
-require './z_get_folder_name' 	# Подключаем процедуру получения адреса топ-папки
-require './z_make_tree'					# Создание дерева из содержимого в топ-папке
-require './z_safe_info'					# Сохраняем информацию в файл и Хеш
-require './z_analizing_info'		# Анализ прочитанных данных (расфасовка) 
-require './z_display_rezult'		# Вывод результата анализа
+require './def/z_get_folder_name' 	# Подключаем процедуру получения адреса топ-папки
+require './def/z_make_tree'					# Создание дерева из содержимого в топ-папке
+require './def/z_safe_info'					# Сохраняем информацию в файл и Хеш
+require './def/z_analizing_info'		# Анализ прочитанных данных (расфасовка) 
+require './def/z_display_rezult'		# Вывод результата анализа
