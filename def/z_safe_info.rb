24a337e5dfaf58a98c4059e2b3208d7ecc3c3170
OPR-A-RUBin <opr.a.ruby@gmail.com>
2021-02-24 13:12:07 +0400

    To 'safe-info': create folder and create filename for listing
    Работаем с методом 'safe_info':
    - создаём папку для листингов (есле её нет)
    - создаём имя файла из настоящего момента с расширением '.list'

--- def/z_safe_info.rb
+++ def/z_safe_info.rb
@@ -2,4 +2,21 @@
 
 def safe_info
 	puts '3__Safe hach information to file'
+
+	# Создать папку listings для хранения файлов с деревом (Если такой дир-и здесь нет)
+	list_folder = 'listings'
+	Dir.mkdir(list_folder) if !Dir.exist?(list_folder) 
+
+	# Создать имя файла (по "сейчас" дате и времени) с расширением .list
+	listing_file = Time.now.strftime '%Y-%m-%d_%H:%M:%S_OPR' + '.list' 
+	puts listing_file
+
+	# @files.each do |key, value| 
+	# 		puts "#{value} = from = #{key}"
+	# end
+
 end
+
+
+# ---------------------------------------------------------------------
+
