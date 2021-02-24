f8deb8aeae9c36f4fa0f16447e3a161a03b4a78e
OPR-A-RUBin <opr.a.ruby@gmail.com>
2021-02-24 13:40:40 +0400

    To 'safe-info': create record hash to file
    Работаем с методом 'safe_info':
    - создаём записывание информации их Хеша @files в файл .list

--- def/z_safe_info.rb
+++ def/z_safe_info.rb
@@ -7,13 +7,26 @@ def safe_info
 	list_folder = 'listings'
 	Dir.mkdir(list_folder) if !Dir.exist?(list_folder) 
 
-	# Создать имя файла (по "сейчас" дате и времени) с расширением .list
-	listing_file = Time.now.strftime '%Y-%m-%d_%H:%M:%S_OPR' + '.list' 
+	# Создать уникальное имя файла (по "сейчас" дате и времени) с расширением .list
+	listing_file = Time.now.strftime '%Y-%m-%d__%H:%M:%S__' + '.list' 
 	puts listing_file
 
-	# @files.each do |key, value| 
-	# 		puts "#{value} = from = #{key}"
-	# end
+	# Дальше работаем в дирктории 'listings' (Заходим в блок do-end)
+	Dir.chdir(list_folder) do
+	
+		# Открываем файл для записи с уникальным именем, созданным выше
+		output_file = File.open(listing_file, "w") 
+
+		# Перебираем весь Хеш @files и пишем данные в файл (разделитель табуляция)
+		@files.each do |key, value| 
+			output_file.write "#{value}\t#{key}\n"
+		end
+
+		# Кончился Хеш. Закрываем файл.
+		output_file.close
+
+		# Закрываем блок. Возвращаемся в исходную директорию
+	end
 
 end
 
